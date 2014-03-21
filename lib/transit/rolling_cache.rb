801e0baa960d0250ccbac0f412156f096d1d43c6
Russ Olsen <russ@russolsen.com>
2014-03-21 17:32:45 -0400

    Rolling cache impl that matches my current understanding

--- lib/transit/rolling_cache.rb
+++ lib/transit/rolling_cache.rb
@@ -1,64 +1,71 @@
-class RollingCache
-  FIRST_ORD = 33
-  CACHE_SIZE = 96
-
-  def initialize
-    clear
-  end
-
-  def [](key)
-    @key_to_value.fetch(key, key)
-  end
-
-  def cache(value)
-    return value unless eligable?(value)
-
-    key = @value_to_key[value]
-    return key if key
-
-
-    encache(value)
-    value
-  end
-
-  def clear
-    #puts "clear cache!"
-
-    @key_to_value = {}
-    @value_to_key = {}
-  end
-
-  def size
-    @key_to_value.size
-  end
-
-  def cache_full?
-    @key_to_value.size >= CACHE_SIZE
-  end
-
-
-  private
-
-  def encache(value)
-    #puts "encache: #{value}"
-    clear if cache_full?
-
-    i = @key_to_value.size
-    key = encode(i)
-    @key_to_value[key] = value
-    @value_to_key[value] = key
-    key
-  end
-
-  def eligable?(value)
-    value.size >= 3
-  end
-
-  def encode(i)
-    "^#{(i+33).chr}"
-  end
-
-  def decode(s)
-    s[1].ord - 33
+module Transit
+  class RollingCache
+    FIRST_ORD = 33
+    CACHE_SIZE = 96
+  
+    def initialize
+      clear
+    end
+  
+    # Always returns the name
+    def decode(name)
+      return @key_to_value[name] if cache_key?(name)
+      return name unless eligable?(name)
+      encache(name)
+    end
+  
+    # Returns the name the first time and the key after that
+    def encode(name)
+      key = @value_to_key[name]
+      if key
+        return key
+      end
+      return name unless eligable?(name)
+      encache(name)
+    end
+  
+    def cache_key?(name)
+      name[0] == '^'
+    end
+  
+    def clear
+      @key_to_value = {}
+      @value_to_key = {}
+    end
+  
+    def size
+      @key_to_value.size
+    end
+  
+    def cache_full?
+      @key_to_value.size >= CACHE_SIZE
+    end
+  
+    private
+  
+    def encache(name)
+      clear if cache_full?
+  
+      existing_key = @value_to_key[name]
+      return existing_key if existing_key
+  
+      i = @key_to_value.size
+      key = encode_key(i)
+      @key_to_value[key] = name
+      @value_to_key[name] = key
+      name
+    end
+  
+    def eligable?(value)
+      value.size >= 3
+    end
+  
+    def encode_key(i)
+      "^#{(i+33).chr}"
+    end
+  
+    def decode_key(s)
+      s[1].ord - 33
+    end
   end
 end
--- spec/transit/rolling_cache_spec.rb
+++ spec/transit/rolling_cache_spec.rb
@@ -1,59 +1,97 @@
 require 'spec_helper'
 
-describe RollingCache do
+module Transit
+  describe RollingCache do
 
-  it 'returns the identifier the first time it sees it' do
-    assert{ RollingCache.new.cache('abcd') == 'abcd' }
-  end
-
-  it 'keeps track of the number of items cached' do
-    rc = RollingCache.new
-    rc.cache('abcd')
-    rc.cache('xxxx')
-    rc.cache('yyyy')
-    assert {rc.size == 3}
-  end
-
-  it 'returns a cache id the 2nd thru n times it sees an identifier' do
-    rc = RollingCache.new
-    assert { rc.cache('abc') == 'abc' }
-
-    key = rc.cache('abc')
-    assert { key == '^!' }
-
-    100.times do
-      assert{ rc.cache('abc') == key }
-    end
-  end
-
-  it 'can handle 96 different values' do
-    rc = RollingCache.new
-    96.times do |i|
-      assert{ rc.cache("value#{i}") == "value#{i}" }
+    it 'returns the identifier the first time it sees it' do
+      assert{ RollingCache.new.encode('abcd') == 'abcd' }
     end
 
-    assert { rc.size == RollingCache::CACHE_SIZE }
-  end
-
-  it 'can resets after 96 different values' do
-    rc = RollingCache.new
-    97.times do |i|
-      assert{ rc.cache("value#{i}") == "value#{i}" }
+    it 'keeps track of the number of items encoded' do
+      rc = RollingCache.new
+      rc.encode('abcd')
+      rc.encode('xxxx')
+      rc.encode('yyyy')
+      assert {rc.size == 3}
     end
 
-    assert { rc.size == 1 }
-  end
+    it 'returns a encode id the 2nd thru n times it sees an identifier' do
+      rc = RollingCache.new
+      assert { rc.encode('abc') == 'abc' }
 
-  it 'Returns the value given a cache key' do
-    rc = RollingCache.new
-    rc.cache 'abcd'
-    key = rc.cache 'abcd'
-    assert { rc[key] == 'abcd' }
-  end
+      key = rc.encode('abc')
+      assert { key == '^!' }
 
-  it 'Returns the key given it if the key is not in the cache' do
-    rc = RollingCache.new
-    assert { rc['abcde'] == 'abcde' }
-  end
+      100.times do
+        assert{ rc.encode('abc') == key }
+      end
+    end
 
+    it 'can handle 96 different values' do
+      rc = RollingCache.new
+      96.times do |i|
+        assert{ rc.encode("value#{i}") == "value#{i}" }
+      end
+
+      assert { rc.size == RollingCache::CACHE_SIZE }
+    end
+
+    it 'can resets after 96 different values' do
+      rc = RollingCache.new
+      (RollingCache::CACHE_SIZE+1).times do |i|
+        assert{ rc.encode("value#{i}") == "value#{i}" }
+      end
+
+      assert { rc.size == 1 }
+    end
+
+    it 'Returns the value given a key' do
+      rc = RollingCache.new
+      rc.encode 'abcd'
+      key = rc.encode 'abcd'
+      assert { rc.decode(key) == 'abcd' }
+    end
+
+    it 'Returns the key given it if the key is not in the cache' do
+      rc = RollingCache.new
+      assert { rc.decode('abcde') == 'abcde' }
+    end
+
+    it 'Whatever comes out of the encode method will work as a key' do
+      sender = RollingCache.new
+      reciever = RollingCache.new
+
+      names = random_strings(200, 10)
+
+      2000.times do |i|
+        name = names.sample
+        assert { reciever.decode(sender.encode(name)) == name }
+      end
+    end
+
+    it 'works consistently for small strings which are not cached' do
+      sender = RollingCache.new
+      reciever = RollingCache.new
+
+      names = random_strings(3, 500)
+      2000.times do |i|
+        name = names.sample
+        assert { reciever.decode(sender.encode(name)) == name }
+      end
+    end
+
+    it 'keeps the cache at no more than its max size' do
+      sender = RollingCache.new
+      reciever = RollingCache.new
+
+      names = random_strings(3, 500)
+      2000.times do |i|
+        name = names.sample
+        reciever.decode(sender.encode(name))
+	assert { sender.size <= RollingCache::CACHE_SIZE }
+	assert { reciever.size <= RollingCache::CACHE_SIZE }
+      end
+    end
+
+  end
 end
