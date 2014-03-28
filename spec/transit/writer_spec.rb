a8dff6d63779faa710647481bcd94103570af558
David Chelimsky and Yoko Harada <pair+dc+yh@thinkrelevance.com>
2014-03-28 16:20:39 -0400

    Initial support for maps
    - monkey patching OJ in order to be able to encode keys
      and values separately

--- spec/transit/writer_spec.rb
+++ spec/transit/writer_spec.rb
@@ -12,6 +12,7 @@ module Transit
       @handlers[Time] = InstantHandler.new
       @handlers[Fixnum] = IntHandler.new
       @handlers[Array] = ArrayHandler.new
+      @handlers[Hash] = MapHandler.new
     end
 
     def [](obj)
@@ -40,6 +41,41 @@ module Transit
       def tag(a) :array end
       def rep(a) a end
       def string_rep(a) nil end
+    end
+
+    class MapHandler
+      def tag(m) :map end
+      def rep(m) m end
+      def string_rep(m) nil end
+    end
+  end
+
+  class Oj::StreamWriter
+    alias orig_push_value  push_value
+
+    def push_value(v, k=nil)
+      if @pushing_map_entry
+        if @map_key
+          @map_value = v
+        else
+          @map_key = v
+        end
+      else
+        if k
+          orig_push_value(v, k)
+        else
+          orig_push_value(v)
+        end
+      end
+    end
+
+    def push_map_entry
+      @pushing_map_entry = true
+    end
+
+    def flush_map_entry
+      orig_push_value(@map_value, @map_key)
+      @pushing_map_entry = @map_value = @map_key = nil
     end
   end
 
@@ -71,6 +107,17 @@ module Transit
       @oj.pop
     end
 
+    def emit_map(obj, _as_map_key_, _cache_)
+      @oj.push_object
+      obj.each do |k, v|
+        @oj.push_map_entry
+        marshal(k, true, _cache_)
+        marshal(v, false, _cache_)
+        @oj.flush_map_entry
+      end
+      @oj.pop
+    end
+
     def marshal(obj, _as_map_key_, _cache_)
       handler = @handlers[obj]
       tag = handler.tag(obj)
@@ -82,6 +129,8 @@ module Transit
         emit_int(rep, _as_map_key_, _cache_)
       when :array
         emit_array(rep, _as_map_key_, _cache_)
+      when :map
+        emit_map(rep, _as_map_key_, _cache_)
       else
         emit_encoded(tag, obj, _as_map_key_, _cache_)
       end
@@ -110,39 +159,52 @@ module Transit
 end
 
 module Transit
-  describe JsonWriter do
-    let(:io) { StringIO.new }
-    let(:writer) { Writer.new(io, :json) }
+  describe Writer do
+    describe :json, :focus do
+      let(:io) { StringIO.new }
+      let(:writer) { Writer.new(io, :json) }
 
-    it "marshals a string" do
-      writer.write("this")
-      assert { io.string == "\"this\"" }
-    end
+      it "marshals a string" do
+        writer.write("this")
+        assert { io.string == "\"this\"" }
+      end
 
-    it "escapes a string that begins with ~" do
-      writer.write("~this")
-      assert { io.string == "\"~~this\"" }
-    end
+      it "escapes a string that begins with ~" do
+        writer.write("~this")
+        assert { io.string == "\"~~this\"" }
+      end
 
-    it "marshals an instant" do
-      t = Time.now
-      writer.write(t)
-      assert { io.string == "\"~t#{t.strftime("%FT%H:%M:%S.%LZ")}\"" }
-    end
+      it "marshals an instant" do
+        t = Time.now
+        writer.write(t)
+        assert { io.string == "\"~t#{t.strftime("%FT%H:%M:%S.%LZ")}\"" }
+      end
 
-    it "marshals an int" do
-      writer.write(37)
-      assert { io.string == "37" }
-    end
+      it "marshals an int" do
+        writer.write(37)
+        assert { io.string == "37" }
+      end
 
-    it "marshals an array" do
-      writer.write([1])
-      assert { io.string == "[1]" }
-    end
+      it "marshals an array" do
+        writer.write([1])
+        assert { io.string == "[1]" }
+      end
 
-    it "marshals an array with several elements including nested arrays" do
-      writer.write([1, "2", [3, ["~4"]]])
-      assert { io.string == "[1,\"2\",[3,[\"~~4\"]]]" }
+      it "marshals an array with several elements including nested arrays" do
+        writer.write([1, "2", [3, ["~4"]]])
+        assert { io.string == "[1,\"2\",[3,[\"~~4\"]]]" }
+      end
+
+      it "marshals a map w/ string keys" do
+        writer.write({"a" => 1, "b" => "c"})
+        assert { io.string == "{\"a\":1,\"b\":\"c\"}" }
+      end
+
+      it "marshals a map w/ string keys that require escaping" do
+        writer.write({"~a" => 1, "b" => "c"})
+        assert { io.string == "{\"~~a\":1,\"b\":\"c\"}" }
+      end
     end
   end
 end
+
