8628efa38e0cd04cd9f58db2e1184d55d7fcf136
David Chelimsky <dchelimsky@gmail.com>
2014-03-28 06:24:08 -0500

    Placeholder for more complete round trip spec

---
+++ spec/transit_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe Transit do
+=begin
+  shared_examples "round trip" do
+    it "round trips nil"
+    it "round trips a keyword"
+    it "round trips a string"
+    it "round trips a bool"
+    it "round trips a long/int/short/byte/bigint"
+    it "round trips a float/double"
+    it "round trips a bigdec"
+    it "round trips an instant"
+    it "round trips a uuid"
+    it "round trips a uri"
+    it "round trips a bytearray"
+    it "round trips a symbol"
+    it "round trips a vector ???"
+    it "round trips a hash w/ stringable keys"
+    it "round trips a set"
+    it "round trips a list (array)"
+    it "round trips a typed array ???"
+    it "round trips a char"
+    it "round trips an extension scalar"
+    it "round trips an extension struct"
+  end
+
+  describe "round trip with json" do
+    include_examples "round trip", :json
+  end
+
+  describe "round trip with msgpack" do
+    include_examples "round trip", :msgpack
+  end
+=end
+end
