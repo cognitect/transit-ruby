a3f99f511728f309eac5867fd85cd35bdf9f39eb
David Chelimsky <dchelimsky@gmail.com>
2014-03-29 11:38:38 -0500

    Group writer examples by leaf node and collections

--- spec/transit/writer_spec.rb
+++ spec/transit/writer_spec.rb
@@ -56,19 +56,18 @@ module Transit
       def string_rep(s) rep(s) end
     end
 
-# Symbol
-# TransitSymbol
-# Keyword (sep from symbol because it might be namespaced - not supported in ruby)
-# String
-# Fixnum
-# NilClass
-# TrueClass
-# FalseClass
-# Float
-# Bignum
-# BigDecimal
-# ByteArray
-# URI
+    # TransitSymbol
+    # Keyword (sep from symbol because it might be namespaced - not supported in ruby)
+    # String
+    # Fixnum
+    # NilClass
+    # TrueClass
+    # FalseClass
+    # Float
+    # Bignum
+    # BigDecimal
+    # ByteArray
+    # URI
 
   end
 
@@ -165,81 +164,90 @@ module Transit
     let(:io) { StringIO.new }
     let(:writer) { Writer.new(io, :json) }
 
-    it "marshals a string" do
-      writer.write("this")
-      assert { io.string == "\"this\"" }
+    describe "leaf nodes" do
+      it "marshals a string" do
+        writer.write("this")
+        assert { io.string == "\"this\"" }
+      end
+
+      it "escapes a string that begins with ~" do
+        writer.write("~this")
+        assert { io.string == "\"~~this\"" }
+      end
+
+      it "marshals an instant" do
+        t = Time.now
+        writer.write(t)
+        assert { io.string == "\"~t#{t.strftime("%FT%H:%M:%S.%LZ")}\"" }
+      end
+
+      it "marshals an int" do
+        writer.write(37)
+        assert { io.string == "37" }
+      end
+
+      it "marshals an array" do
+        writer.write([1])
+        assert { io.string == "[1]" }
+      end
+
+      it "marshals a Ruby Symbol" do
+        writer.write(:this)
+        assert { io.string == "\"~:this\"" }
+      end
+
+      it "marshals a namespaced Ruby Symbol" do
+        writer.write(:"namespace/name")
+        assert { io.string == "\"~:namespace/name\"" }
+      end
     end
 
-    it "escapes a string that begins with ~" do
-      writer.write("~this")
-      assert { io.string == "\"~~this\"" }
-    end
+    describe "collections" do
+      it "marshals an array with several elements including nested arrays" do
+        writer.write([1, "2", [3, ["~4"]]])
+        assert { io.string == "[1,\"2\",[3,[\"~~4\"]]]" }
+      end
 
-    it "marshals an instant" do
-      t = Time.now
-      writer.write(t)
-      assert { io.string == "\"~t#{t.strftime("%FT%H:%M:%S.%LZ")}\"" }
-    end
+      it "marshals a map w/ string keys" do
+        writer.write({"a" => 1, "b" => "c"})
+        assert { io.string == "{\"a\":1,\"b\":\"c\"}" }
+      end
 
-    it "marshals an int" do
-      writer.write(37)
-      assert { io.string == "37" }
-    end
+      it "marshals a map w/ string keys and values that require escaping" do
+        writer.write({"~a" => 1, "~b" => "~c"})
+        assert { io.string == "{\"~~a\":1,\"~~b\":\"~~c\"}" }
+      end
 
-    it "marshals an array" do
-      writer.write([1])
-      assert { io.string == "[1]" }
-    end
+      it "marshals a map w/ Symbol keys and vals" do
+        writer.write({:a => 1, b: :c})
+        assert { io.string == "{\"~:a\":1,\"~:b\":\"~:c\"}" }
+      end
 
-    it "marshals a Ruby Symbol" do
-      writer.write(:this)
-      assert { io.string == "\"~:this\"" }
-    end
+      it "marshals a map w/ time keys" do
+        t = Time.new(2014,1,2,3,4,5)
+        writer.write({t => "ignore"})
+        assert { io.string == "{\"~t2014-01-02T03:04:05.000Z\":\"ignore\"}" }
+      end
 
-    it "marshals an array with several elements including nested arrays" do
-      writer.write([1, "2", [3, ["~4"]]])
-      assert { io.string == "[1,\"2\",[3,[\"~~4\"]]]" }
-    end
+      it "marshals a nested map" do
+        t = Time.new(2014,1,2,3,4,5)
+        writer.write({:a => { t => :ignore }})
+        assert { io.string == "{\"~:a\":{\"~t2014-01-02T03:04:05.000Z\":\"~:ignore\"}}" }
+      end
 
-    it "marshals a map w/ string keys" do
-      writer.write({"a" => 1, "b" => "c"})
-      assert { io.string == "{\"a\":1,\"b\":\"c\"}" }
-    end
+      it "raises for non-stringable map keys" do
+        assert { rescuing { writer.write({[1,2] => "ignore"}).message =~ /Can not push/ } }
+      end
 
-    it "marshals a map w/ string keys and values that require escaping" do
-      writer.write({"~a" => 1, "~b" => "~c"})
-      assert { io.string == "{\"~~a\":1,\"~~b\":\"~~c\"}" }
-    end
+      it "marshals a nested data structure within a map" do
+        writer.write({a: [1, [{b: "~c"}]]})
+        assert { io.string == "{\"~:a\":[1,[{\"~:b\":\"~~c\"}]]}" }
+      end
 
-    it "marshals a map w/ Symbol keys and vals" do
-      writer.write({:a => 1, b: :c})
-      assert { io.string == "{\"~:a\":1,\"~:b\":\"~:c\"}" }
-    end
-
-    it "marshals a map w/ time keys" do
-      t = Time.new(2014,1,2,3,4,5)
-      writer.write({t => "ignore"})
-      assert { io.string == "{\"~t2014-01-02T03:04:05.000Z\":\"ignore\"}" }
-    end
-
-    it "marshals a nested map" do
-      t = Time.new(2014,1,2,3,4,5)
-      writer.write({:a => { t => :ignore }})
-      assert { io.string == "{\"~:a\":{\"~t2014-01-02T03:04:05.000Z\":\"~:ignore\"}}" }
-    end
-
-    it "raises for non-stringable map keys" do
-      assert { rescuing { writer.write({[1,2] => "ignore"}).message =~ /Can not push/ } }
-    end
-
-    it "marshals a nested data structure within a map" do
-      writer.write({a: [1, [{b: "~c"}]]})
-      assert { io.string == "{\"~:a\":[1,[{\"~:b\":\"~~c\"}]]}" }
-    end
-
-    it "marshals a nested data structure within an array" do
-      writer.write([37, {a: [1, [{b: "~c"}]]}])
-      assert { io.string == "[37,{\"~:a\":[1,[{\"~:b\":\"~~c\"}]]}]" }
+      it "marshals a nested data structure within an array" do
+        writer.write([37, {a: [1, [{b: "~c"}]]}])
+        assert { io.string == "[37,{\"~:a\":[1,[{\"~:b\":\"~~c\"}]]}]" }
+      end
     end
   end
 end
