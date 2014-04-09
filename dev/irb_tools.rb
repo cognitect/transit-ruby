61c70af907bf5228c537c9e9620a83634dae551c
Russ Olsen <russ@russolsen.com>
2014-04-09 11:00:19 -0400

    Added handy monkey patching to go to and from transit format

--- dev/irb_tools.rb
+++ dev/irb_tools.rb
@@ -1,5 +1,22 @@
+require 'stringio'
+
 def time
   start = Time.now
   yield
   puts "Elapsed: #{Time.now - start}"
+end
+
+class Object
+  def to_transit(format=:json)
+    sio = StringIO.new
+    Transit::Writer.new(sio, format).write(self)
+    sio.string
+  end
+end
+
+class String
+  def from_transit
+    sio = StringIO.new(self)
+    Transit::Reader.new(:json).read(sio)
+  end
 end
