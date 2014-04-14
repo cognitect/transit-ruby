8cd563eb07e6d13ba1e8d4dcd3f4c436828d4e4f
David Chelimsky <dchelimsky@gmail.com>
2014-04-13 21:47:04 -0700

    add benchmark results to benchmark file

--- benchmarks/encoding_time.rb
+++ benchmarks/encoding_time.rb
@@ -3,8 +3,8 @@ require 'transit'
 require 'benchmark'
 
 decoder = Transit::Decoder.new
-special_decoder = Transit::Decoder.new
-special_decoder.register("t") {|t| Transit::Util.time_from_millis(t.to_i)}
+custom_decoder = Transit::Decoder.new
+custom_decoder.register("t") {|t| Transit::Util.time_from_millis(t.to_i)}
 
 n = 10000
 
@@ -12,18 +12,23 @@ t = Time.now
 m = Transit::Util.time_to_millis(t)
 m_to_s = m.to_s
 s = t.utc.iso8601(3)
-c = Transit::RollingCache.new
 
-def parse(rep)
-  if rep =~ /Z$/
-    Time.parse(rep).utc
-  else
-    Transit::Util.time_from_millis(rep.to_i).utc
-  end
+results = [Time.parse(s).utc,
+           Transit::Util.time_from_millis(m),
+           Transit::Util.time_from_millis(m_to_s.to_i),
+           decoder.decode("~t#{s}"),
+           decoder.decode({"~#t" => m}),
+           custom_decoder.decode("~t#{m}")]
+
+as_millis = results.map {|r| Transit::Util.time_to_millis(r)}
+
+if Set.new(as_millis).length > 1
+  warn "Not all methods returned the same values:"
+  warn as_millis.to_s
 end
 
 Benchmark.benchmark do |bm|
-  puts "Time from #{s.inspect}"
+  puts "Time.parse(#{s.inspect}).utc"
   3.times do
     bm.report do
       n.times do
@@ -33,57 +38,90 @@ Benchmark.benchmark do |bm|
   end
 
   puts
-
-  puts "Time from #{m_to_s.inspect}"
+  puts "Transit::Util.time_from_millis(#{m.inspect})"
   3.times do
     bm.report do
       n.times do
-        Transit::Util.time_from_millis(m_to_s.to_i).utc
+        Transit::Util.time_from_millis(m)
       end
     end
   end
 
   puts
-
-  puts "Time from #{m.inspect}"
+  puts "Transit::Util.time_from_millis(#{m_to_s.inspect}.to_i)"
   3.times do
     bm.report do
       n.times do
-        Transit::Util.time_from_millis(m).utc
+        Transit::Util.time_from_millis(m_to_s.to_i)
       end
     end
   end
 
   puts
-
   puts "decoder.decode(\"~t#{s}\")"
   3.times do
     bm.report do
       n.times do
-        decoder.decode("~t#{s}", c)
+        decoder.decode("~t#{s}")
       end
     end
   end
 
   puts
-
-  puts "decoder.decode(\"~t#{m}\")"
-  3.times do
-    bm.report do
-      n.times do
-        special_decoder.decode("~t#{m}", c)
-      end
-    end
-  end
-
-  puts
-
   puts "decoder.decode({\"~#t\" => #{m}})"
   3.times do
     bm.report do
       n.times do
-        decoder.decode({"~#t" => m}, c)
+        decoder.decode({"~#t" => m})
+      end
+    end
+  end
+
+  puts
+  puts "custom_decoder.decode(\"~t#{m}\")"
+  3.times do
+    bm.report do
+      n.times do
+        custom_decoder.decode("~t#{m}")
       end
     end
   end
 end
+
+__END__
+
+$ ruby benchmarks/encoding_time.rb
+Not all methods returned the same values:
+[1397450386660, 1397450386661, 1397450386661, 1397450386660, 1397450386661, 1397450386661]
+
+# This ^^ shows that Time.parse is truncating millis instead of rounding them.
+
+Time.parse("2014-04-14T04:39:46.660Z").utc
+   0.270000   0.000000   0.270000 (  0.265990)
+   0.260000   0.000000   0.260000 (  0.261357)
+   0.260000   0.000000   0.260000 (  0.263597)
+
+Transit::Util.time_from_millis(1397450386661)
+   0.040000   0.000000   0.040000 (  0.041289)
+   0.040000   0.000000   0.040000 (  0.043084)
+   0.050000   0.000000   0.050000 (  0.043169)
+
+Transit::Util.time_from_millis("1397450386661".to_i)
+   0.040000   0.000000   0.040000 (  0.048342)
+   0.050000   0.000000   0.050000 (  0.047006)
+   0.050000   0.010000   0.060000 (  0.046771)
+
+decoder.decode("~t2014-04-14T04:39:46.660Z")
+   0.310000   0.000000   0.310000 (  0.311126)
+   0.310000   0.000000   0.310000 (  0.312943)
+   0.320000   0.000000   0.320000 (  0.317080)
+
+decoder.decode({"~#t" => 1397450386661})
+   0.080000   0.000000   0.080000 (  0.081899)
+   0.070000   0.000000   0.070000 (  0.077323)
+   0.080000   0.000000   0.080000 (  0.079400)
+
+custom_decoder.decode("~t1397450386661")
+   0.080000   0.000000   0.080000 (  0.074236)
+   0.080000   0.000000   0.080000 (  0.080308)
+   0.070000   0.000000   0.070000 (  0.075567)
