37a13321311c90ac941a2b8278e9e1789e61fc0e
David Chelimsky <pair+dc@cognitect.com>
2014-04-18 20:35:00 -0500

    Added failing example of generating a UUID from negative numbers

--- spec/transit/transit_types_spec.rb
+++ spec/transit/transit_types_spec.rb
@@ -62,5 +62,20 @@ module Transit
       assert { UUID.from_ints(ints) == uuid }
       assert { UUID.from_string(s)  == uuid }
     end
+
+    # These next two examples both work in clojure, and produce the
+    # same UUID string. The positive example passes, but the negative
+    # example fails.
+    it 'supports positive ints' do
+      ints = [15122072677373264971, 11503552724641936009]
+      uuid = UUID.from_ints(ints)
+      assert { uuid.to_s == "d1dc64fa-da79-444b-9fa4-d4412f427289" }
+    end
+
+    it 'supports negative ints' do
+      ints = [-3324671396336286645, -6943191349067615607]
+      uuid = UUID.from_ints(ints)
+      assert { uuid.to_s == "d1dc64fa-da79-444b-9fa4-d4412f427289" }
+    end
   end
 end
