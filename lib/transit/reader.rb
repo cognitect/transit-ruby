c9ae914224d0f28efcf2e8ce677aa85f40c51750
David Chelimsky <dchelimsky@gmail.com>
2014-06-29 21:12:09 -0500

    Back to yalj-ruby for reading (for now)
    - it's slower than oj, however
    - bin/verify was timing out with oj - don't know why

--- lib/transit/reader.rb
+++ lib/transit/reader.rb
@@ -3,49 +3,27 @@
 
 module Transit
   class JsonUnmarshaler
-    class Handler
-      def each(&block)
-        @each = block
-      end
-
-      def hash_start
-        {}
-      end
-
-      def hash_set(h,k,v)
-        h.store(k,v)
-      end
-
-      def array_start
-        []
-      end
-
-      def array_append(a,v)
-        a << v
-      end
-
-      def add_value(v)
-        @each[v] if @each
-      end
-
-      def error(message, line, column)
-        raise Exception.new(message, line, column)
-      end
-    end
+    CHUNK_SIZE = 8192
 
     def initialize(io, opts)
       @io = io
+      @yajl = Yajl::Parser.new
       @decoder = Transit::Decoder.new(opts)
-      @handler = Handler.new
     end
 
     def read
       if block_given?
-        @handler.each {|v| yield @decoder.decode(v)}
+        @yajl.on_parse_complete = ->(obj){ yield @decoder.decode(obj) }
+        while true
+          begin
+            @yajl << @io.readpartial(CHUNK_SIZE)
+          rescue EOFError => e
+            break
+          end
+        end
       else
-        @handler.each {|v| return @decoder.decode(v)}
+        @decoder.decode(@yajl.parse(@io))
       end
-      Oj.sc_parse(@handler, @io)
     end
   end
 
@@ -72,7 +50,7 @@ module Transit
     def initialize(type, io, opts={})
       @reader = case type
                 when :json, :json_verbose
-                  require 'oj'
+                  require 'yajl'
                   JsonUnmarshaler.new(io, opts)
                 else
                   require 'msgpack'
